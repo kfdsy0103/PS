e2e7fcded75bb98ba163fca714eccfb0c1802f44
Pywoo <81312085+kfdsy0103@users.noreply.github.com>
2024-07-24 17:54:54 +0900

    Add files via upload

--- 19942.cpp
+++ 19942.cpp
@@ -1,61 +1,31 @@
-// 불리언 배열 역할을 하는 하나의 숫자를 만들어
-// 비트 연산자를 통해 탐색, 수정 등의 작업을 함
-// 모든 조합을 따질 때 사용하면 유용하다.
-// int 형의 한계가 있으므로 n이 30 이하면 고려해볼만 하다.
+// 초기화, 기저사례, 메모이제이션, 로직
+// 완탐 > 경우의 수가 너무 크다 > 배열에 담을 수 있는지 > dp로 풀이
 #include <bits/stdc++.h>
 
 using namespace std;
 
 const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
-int n, mp, mf, ms, mv, cost;
-int p, f, s, v, c;
-int ret = 99999999;
-map<int, vector<vector<int>>> m;
+int n, m;
+int a[1004];
+int dp[1004][2][34]; // 특정 상태값에서 얻을 수 있는 나무의 최대 개수
 
-struct ss {
-	int p, f, s, v, c;
-}a[16];
+int go(int idx, int pos, int cnt) {
+	if (cnt < 0) return -1e9;
+	if (idx == n) return 0;
+
+	int& ret = dp[idx][pos][cnt];
+	if (~ret) return ret;
+	return ret = max(go(idx + 1, pos ^ 1, cnt - 1), go(idx + 1, pos, cnt)) + (a[idx] == pos + 1);
+}
 
 int main() {
-	cin >> n;
-	cin >> mp >> mf >> ms >> mv;
+	memset(dp, -1, sizeof(dp));
+	cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		cin >> a[i].p >> a[i].f >> a[i].s >> a[i].v >> a[i].c;
-	}
+	for (int i = 0; i < n; i++) cin >> a[i];
 
-	for (int i = 1; i < (1 << n); i++) { // 1, 2, ..., n번째 비트를 켜서 후보군에 넣기
-		p = f = s = v = c = 0;
-		vector<int> li;
-		for (int j = 0; j < n; j++) { // 몇 번째 비트가 후보군인지 체크
-			if (i & (1 << j)) {
-				p += a[j].p;
-				f += a[j].f;
-				s += a[j].s;
-				v += a[j].v;
-				c += a[j].c;
-				li.push_back(j + 1);
-			}
-		}
-		if (p >= mp && f >= mf && s >= ms && v >= mv) {
-			if (c <= ret) {
-				ret = c;
-				m[ret].push_back(li);
-			}
-		}
-	}
-
-	if (ret == 99999999) {
-		cout << "-1\n";
-	}
-	else {
-		sort(m[ret].begin(), m[ret].end());
-		cout << ret << "\n";
-		for (int a : m[ret][0]) {
-			cout << a << " ";
-		}
-	}
+	cout << max(go(0, 0, m), go(0, 1, m - 1));
 
 	return 0;
 }
