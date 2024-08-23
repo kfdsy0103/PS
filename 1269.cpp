5a91965087cd5235f4d6bc29970986444277fdeb
Pywoo <81312085+kfdsy0103@users.noreply.github.com>
2024-08-24 00:34:23 +0900

    Add files via upload

--- 1269.cpp
+++ 1269.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct room {
+    long long t;
+    long long a;
+    long long h;
+};
+
 const int dy[4] = { -1,1,0,0 };
 const int dx[4] = { 0,0,-1,1 };
-int n, m, num, ret;
-map<int, int> mp;
+long long n, atk, ret, lo, hi;
+room arr[123457];
 
 void init() {
     ios_base::sync_with_stdio(false);
@@ -12,21 +18,45 @@ void init() {
     cout.tie(NULL);
 }
 
+bool check(long long mid) {
+    long long attack = atk;
+    long long hp = mid;
+    for (int i = 0; i < n; i++) {
+        if (arr[i].t == 1) {
+            long long cnt = arr[i].h / attack;
+            if (arr[i].h % attack) cnt++;
+            hp -= arr[i].a * (cnt - 1);
+        }
+        else {
+            attack += arr[i].a;
+            hp = min(mid, hp + arr[i].h);
+        }
+        if (hp <= 0) return false;
+    }
+    return true;
+}
+
 int main() {
     init();
-    cin >> n >> m;
+    cin >> n >> atk;
+
     for (int i = 0; i < n; i++) {
-        cin >> num;
-        mp[num]++;
-    }
-    for (int i = 0; i < m; i++) {
-        cin >> num;
-        mp[num]++;
-    }
-    for (auto it : mp) {
-        if (it.second == 1) ret++;
+        cin >> arr[i].t >> arr[i].a >> arr[i].h;
     }
 
+    lo = 1;
+    hi = 1e18;
+    while (lo <= hi) {
+        long long mid = (lo + hi) / 2;
+        if (check(mid)) {
+            hi = mid - 1;
+            ret = mid;
+        }
+        else {
+            lo = mid + 1;
+        }
+    }
+    
     cout << ret;
 
     return 0;
